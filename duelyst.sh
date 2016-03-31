369d88f1910be7e519f3c893e2a6083eddefabde
Dylan Zahn <dylan.zahn@gmail.com>
2016-03-30 20:30:54 -0500

    add script for Duelyst

--- duelyst.sh
+++ duelyst.sh
@@ -2,7 +2,7 @@
 # This simply runs Duelyst in a Chromium "app" window so it appears naturally like a native app. Useful for window managers.
 #Created by: Dylan Zahn
 
-#Use this by sending it to /usr/local/bin/
-#It will then show up by any application launcher
+#Use this by simply typing in "bash duelyst.sh" or by sending it to /usr/local/bin/
+#It will then show up by any application launcher. I personally use rofi
 
 exec chromium --new-window --app=http://beta.duelyst.com
